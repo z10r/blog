23cd688239119fb218153ea3dbec3c211bd5487f
z10r <57376153+z10r@users.noreply.github.com>
2020-08-08 17:19:57 -0700

    Update systeminfo.bat

--- systeminfo.bat
+++ systeminfo.bat
@@ -1,6 +1,6 @@
 @ECHO OFF 
 :: This batch file reveals OS, hardware, and networking configuration.
-TITLE CDI SYSTEM INFO
+TITLE SYSTEM INFO
 ECHO Please wait... Checking system information.
 :: Section 1: OS information.
 ECHO ============================
@@ -21,4 +21,5 @@ ECHO LEXRTG MTYGG
 ECHO ============================
 ipconfig | findstr IPv4
 ipconfig | findstr IPv6
+timeout 5
 PAUSE
